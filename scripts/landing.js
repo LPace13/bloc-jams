3e208a96f13fba15901905538996a495cedfb443
Lesley <lesley.pace13@gmail.com>
2016-12-22 17:18:49 -0700

    Checkpoint 8 Javascript update final to animate points upon scroll.

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,11 +1,23 @@
-var animatePoints = function () {
-    var points = document.getElementsByClassName('point');
-    
-    var revealPoint = function (index) {
-        for (var i = 0; i < points.length; i++) {
-        revealPoint(i);
-        points[index].style.opacity = 1;
-        points[index].style.transform = "scaleX(1) translateY(0)";
-        points[index].style.msTransform = "scaleX(1) translateY(0)";
-        points[index].style.WebkitTransform = "scaleX(1) translateY(0)";
-    };
+var pointsArray = document.getElementsByClassName('point');
+var revealPoint = function (points, index) {
+    points[index].style.opacity = 1;
+    points[index].style.transform = "scaleX(1) translateY(0)";
+    points[index].style.msTransform = "scaleX(1) translateY(0)";
+    points[index].style.WebkitTransform = "scaleX(1) translateY(0)";
+};
+var animatePoints = function (points) {
+    //for loop
+        // reveal point for each point 
+    for (var i = 0; i < points.length; i++) {
+            revealPoint(points, i);
+    }
+};
+window.onload = function() {
+    var sellingPoints = document.getElementsByClassName('selling-points') [0];
+    var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
+    window.addEventListener('scroll', function(event) {
+        if (document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance) {
+                animatePoints(pointsArray);
+        }
+    });
+}
